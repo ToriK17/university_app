71ab88ef39ee26d11d0d9851dd5c040a90c9c7fe
ToriK17 <vfcad@protonmail.com>
2019-10-05 15:25:22 -0700

    changes column name in courses and adds dummy data in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Course.create!([
+#   {num: 201 , name: "Financial Accounting", department: "Accounting"}, 
+#   {num: 202, name: "Managerial Accounting", department: "Accounting"}, 
+#   {num: 400, name: "The Accounting Environment", department: "Accounting"}, 
+#   {num: 401, name: "Financial Reporting I", department: "Accounting"}
+# ])
+
+# User.create!([
+#   {user_name: "Jeff2015", email: "jwinger@gmail.com", password_digest: "password"}, 
+#   {user_name: "TroyB", email: "datroy@gmail.com", password_digest: "password"},
+#   {user_name: "BenCheng", email: "senorcheng@gmail.com", password_digest: "password"},
+#   {user_name: "MzBennett", email: "ShirleyB@gmail.com", password_digest: "password"}
+# ])
+
+# Resource.create!([
+#   {name: "book"}, 
+#   {name: "midterm"}, 
+#   {name: "Final"}, 
+#   {name: "Knowledge Nuggets"}
+# ])
+
+
+
