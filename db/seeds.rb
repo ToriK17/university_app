4dbd9084c93966f672348b93bf9566decbff8f01
ToriK17 <vfcad@protonmail.com>
2019-11-14 08:00:09 -0800

    fixes bug with courses index search bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,51 +13,62 @@
 #   {num: 240 , name: "Investigative Journalism", department: "Journalism"},
 #   {num: 240 , name: "Communication Studies", department: "Communication"},
 #   {num: 107 , name: "Basic Genealogy", department: "Biology"},
-#   {num: 107 , name: "Beginner Pottery", department: "Art"},
-#   {num: 107 , name: "The Science of Illusion", department: "Magic"},
-#   {num: 107 , name: "Contemporary American Poultry", department: "Magic"},
-#   {num: 107 , name: "Ladders", department: "Psychology"},
+#   {num: 407 , name: "Beginner Pottery", department: "Art"},
+#   {num: 307 , name: "The Science of Illusion", department: "Magic"},
+#   {num: 109 , name: "Contemporary American Poultry", department: "Magic"},
+#   {num: 307 , name: "Ladders", department: "Psychology"},
 #   {num: 107 , name: "The Art of Discourse", department: "Communication"},
 #   {num: 102 , name: "English as a Second Language", department: "Spanish"},
 #   {num: 101 , name: "Beginner Anthropology", department: "Anthropology"},
 #   {num: 107 , name: "Accounting for Lawyers", department: "Accounting"},
 #   {num: 101 , name: "Basic Rocket Science", department: "Mechanical Engineering"},
-#   {num: 101 , name: "Myths and Ancient Peoples", department: "Theology"},
+#   {num: 103 , name: "Myths and Ancient Peoples", department: "Theology"},
 #   {num: 101 , name: "Epidemiology", department: "janitorial"},
-#   {num: 107 , name: "Learning!", department: "Studies"},
+#   {num: 307 , name: "Learning!", department: "Studies"},
 #   {num: 107 , name: "Conspiracy Theories and Interior Design", department: "Interior Design"},
 #   {num: 212 , name: "Celebrity Pharmacology", department: "Pharmacology"},
-#   {num: 212 , name: "Advanced Dungeons & Dragons", department: "Magic"},
-#   {num: 212 , name: "Intermediate Documentary Filmmaking", department: "Film"},
-#   {num: 212 , name: "Competitive Wine Tasting", department: "Culinary Arts"},
+#   {num: 205 , name: "Advanced Dungeons & Dragons", department: "Magic"},
+#   {num: 202 , name: "Intermediate Documentary Filmmaking", department: "Film"},
+#   {num: 201 , name: "Competitive Wine Tasting", department: "Culinary Arts"},
 #   {num: 212 , name: "Biology 101", department: "Biology"}, 
-#   {num: 212 , name: "Remedial Chaos Theory", department: "Mathematics"},
+#   {num: 222 , name: "Remedial Chaos Theory", department: "Mathematics"},
 #   {num: 212 , name: "Intro to Knots", department: "Survival"}, 
 #   {num: 212 , name: "Intro to Teaching", department: "Education"},
-#   {num: 212 , name: "The Floor is Made of Lava", department: "Geothermal Escapism"},
-#   {num: 212 , name: "Lawnmower Maintenance and Postnatal Care", department: "Geothermal Escapism"},
-#   {num: 212 , name: "Basic Email Security", department: "Computer Science"}  
+#   {num: 301 , name: "The Floor is Made of Lava", department: "Geothermal Escapism"},
+#   {num: 212 , name: "Lawnmower Maintenance and Postnatal Care", department: "Advanced Geothermal Escapism"},
+#   {num: 300 , name: "Basic Email Security", department: "Computer Science"},
+#   {num: 201 , name: "Financial Accounting", department: "Accounting"},
+#   {num: 202 , name: "Managerial Accounting", department: "Accounting"},
+#   {num: 401 , name: "Financial Reporting I", department: "Accounting"}  
 # ])
 
 # User.create!([
 #   {user_name: "Jeff_Winger", email: "j.winger@gmail.com", password_digest: "password"}, 
 #   {user_name: "TroyBarnes", email: "troyb@gmail.com", password_digest: "password"},
 #   {user_name: "Senor_Cheng", email: "senorcheng@gmail.com", password_digest: "password"},
-#   {user_name: "Shirley_Sandwhiches", email: "S.Bennet@gmail.com", password_digest: "password"},
+#   {user_name: "Shirley_B", email: "S.Bennet@gmail.com", password_digest: "password"},
 #   {user_name: "Annie_E", email: "j.winger@gmail.com", password_digest: "password"},
-#   {user_name: "AbedCoolFilms", email: "abed.n@gmail.com", password_digest: "password"},
+#   {user_name: "AbedsCoolFilms", email: "abed.n@gmail.com", password_digest: "password"},
 #   {user_name: "Star_Burns", email: "osbourne@gmail.com", password_digest: "password"},
 #   {user_name: "Star_Burns", email: "osbourne@gmail.com", password_digest: "password"},
-#   {user_name: "Pierce_Hawthorn", email: "hawthorn_wipes@gmail.com", password_digest: "password"}
+#   {user_name: "Pierce_Hawthorn", email: "hawthorn_wipes@gmail.com", password_digest: "password"},
+#   {user_name: "Colonel Kernel", email: "boris@gmail.com", password_digest: "password"},
+#   {user_name: "Apachai", email: "roo@gmail.com", password_digest: "password"},
+#   {user_name: "Freya", email: "destroyerofworlds@gmail.com", password_digest: "password"},
+#   {user_name: "Citra", email: "handofjustice@gmail.com", password_digest: "password"},
+#   {user_name: "mr_bear", email: "bear_rawr_rawr@gmail.com", password_digest: "password"}
 # ])
 
 # Resource.create!([
-#   {name: "Book"}, 
-#   {name: "Midterm"}, 
+#   {name: "Books"},
+#   {name: "Class Notes"}, 
+#   {name: "Midterm"},
+#   {name: "Quizzes"}, 
 #   {name: "Final"},
-#   {name: "Notes"},
-
-#   {name: "Knowledge Nuggets"}
+#   {name: "Recorded Lectures (audio)"},
+#   {name: "Recorded Lectures (video)"}, 
+#   {name: "Specialty Equipment"},
+#   {name: "Knowledge Nuggets"},
 # ])
 
 
